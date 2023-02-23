6be138e470ba2c62466471a61b075a4e8881153c
LloydNicholson <34131158+LloydNicholson@users.noreply.github.com>
2023-02-23 13:31:17 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 var express = require("express");
 var app = express();
-var fs = require("fs");
+
+const users = [
+  {
+    name: "john",
+    password: "password1",
+    profession: "teacher",
+    id: 1,
+  },
+  {
+    name: "james",
+    password: "password2",
+    profession: "librarian",
+    id: 2,
+  },
+  {
+    name: "todd",
+    password: "password3",
+    profession: "clerk",
+    id: 3,
+  },
+];
 
 app.get("/listUsers", function (req, res) {
-  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
-    console.log(data);
-    res.end(data);
-  });
+  return res.json(users);
+});
+
+app.delete("/deleteUser/:userId", function (req, res) {
+  const id = req.params.userId;
+  const indexToDelete = users.findIndex((p) => p.id == id);
+  if (indexToDelete > -1) {
+    users.splice(indexToDelete, 1);
+  }
+  return res.json(users);
 });
 
 var server = app.listen(8081, function () {
